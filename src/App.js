799b7990f4f7809559b32fe5ccfd5cbe703b4f55
Estefania <estefaniatorres1216@gmail.com>
2025-09-23 15:37:03 -0500

    Subiendo proyecto completo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import PaginaInicio from './components/pages/PaginaInicio';
+import BarraNavegacion from './components/pages/BarraNavegacion';
+import Carrusel from './components/pages/Carrusel';
+import BarraPrincipal from './components/pages/BarraPrincipal';
+import ContactenosFooter from './components/pages/ContactenosFooter';
+import InisiarSesion from './components/pages/IniciarSesion';
+import Registrarse from './components/pages/Registrarse';
+import ProductosAtaud from './components/pages/ProductosAtaud';
+import AcercaDeNosotros from './components/pages/AcercaDeNosotros';
+import SideBar from './components/SideBar';
+import BarraCliente from './components/clientes/BarraCliente';
+import Cliente from './components/clientes/Cliente';
+import AgregarCliente from './components/clientes/AgregarCliente';
+import EditarCliente from './components/clientes/EditarCliente';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+   <Router>
+    
+    <SideBar />
+    
+    <div style={{ marginLeft: "200px", background:"#D8CFE8",  height: "auto" }}>
+      <BarraCliente/>
+      
+        <Routes>
+          <Route path='/clientes/Cliente' element={<Cliente/>}></Route>
+          <Route path='/clientes/AgregarCliente' element={<AgregarCliente/>}></Route>
+          <Route path='/clientes/EditarCliente' element={<EditarCliente/>}></Route>
+          
+          
+        </Routes>
+      </div>
+   </Router>
+   </>
   );
 }
 
